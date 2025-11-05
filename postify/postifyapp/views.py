820b60d3718eb5908282d6b8ee6c0684b65e72e5
Meesa000 <104768879+Meesa000@users.noreply.github.com>
2025-11-05 20:20:36 +0000

    Add post display functionality to homepage view

--- postify/postifyapp/views.py
+++ postify/postifyapp/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def homepage(request):
-    return render(request, "index.html")
+    posts = {"post1" : "I guess this is where a random post will appear? It would probably "
+             "be good to have this in the center, similar to twitter."}
+    
+    return render(request, "index.html", posts)
